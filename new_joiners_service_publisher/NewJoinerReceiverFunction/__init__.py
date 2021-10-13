e09fbffbed6a8ee4c11ff4b5036cce08f62c6234
Juan Fernando Rojas <juan.rojas@endava.com>
2021-10-13 15:15:28 -0500

    Remove unused code

--- new_joiners_service_publisher/NewJoinerReceiverFunction/__init__.py
+++ new_joiners_service_publisher/NewJoinerReceiverFunction/__init__.py
@@ -9,37 +9,6 @@ import docx2txt
 import json
 from json import JSONEncoder
 
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
-import logging
-import azure.functions as func
-from itertools import *
-import spacy
-nlp = spacy.load("en_core_web_trf")
-import PyPDF2
-import pika
-import docx2txt
-import json
-from json import JSONEncoder
-
 ALLOWED_EXTENSIONS = {'pdf', 'docx'}
 
 class Ner: 
