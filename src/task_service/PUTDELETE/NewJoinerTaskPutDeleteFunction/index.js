d08aa9e7e65e402315151d41f9d9cf7950835073
Juan Fernando Rojas <juan.rojas@endava.com>
2021-10-22 18:37:20 -0500

    Adding taskqueue message sender

--- src/task_service/PUTDELETE/NewJoinerTaskPutDeleteFunction/index.js
+++ src/task_service/PUTDELETE/NewJoinerTaskPutDeleteFunction/index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 
 const db = require('../dboperations');
+const sender = require('../sender');
 const joinerservice = require('../joinerservice');
+const TaskMessage = require('../taskMessage');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');   
@@ -20,6 +22,15 @@ module.exports = async function (context, req) {
                         
                         var joiner = ''
 
+                        var taskMessage = new TaskMessage(
+                            req.body.Id, 
+                            req.body.Name,  
+                            req.body.Description, 
+                            req.body.EstimatedRequiredHours,
+                            req.body.Stack,
+                            req.body.MinRole
+                            )
+
                         if (req.body.UserId) {
                             
                             joiner = await joinerservice.get(req.body.UserId);
@@ -31,6 +42,8 @@ module.exports = async function (context, req) {
                                     statusCode: 400
                                 }
                             }
+
+                            taskMessage.User = joiner
                         }
 
                         if (!req.body.UserId || joiner) {
@@ -38,13 +51,16 @@ module.exports = async function (context, req) {
                                 hasParent = await db.getParent(req.body.TaskId)
         
                                 if (hasParent.length == 0) {                         
+                                    taskMessage.Task = hasParent
+                                    
                                     await db.update(paramId, req.body)
+                                    await sender.send(taskMessage)
         
                                     context.res = {   
                                         headers: { 'Content-Type': 'application/json' },     
                                         body: '{ "message": "Successfully updated" }',
                                         statusCode: 200
-                                    };
+                                    };                                    
                                 }
         
                                 else {
@@ -57,6 +73,7 @@ module.exports = async function (context, req) {
                             }
                             else {
                                 await db.update(paramId, req.body)
+                                await sender.send(taskMessage)                               
         
                                 context.res = {   
                                     headers: { 'Content-Type': 'application/json' },     
