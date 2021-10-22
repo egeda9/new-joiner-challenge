e374002034038c6952bc1b2644f1a4105975d1cc
Juan Fernando Rojas <juan.rojas@endava.com>
2021-10-22 15:27:07 -0500

    Task service deployment

--- src/task_service/PUTDELETE/joinerservice.js
+++ src/task_service/PUTDELETE/joinerservice.js
@@ -1,9 +1,13 @@
 const axios = require('axios');
 
+const config = {
+    joinerBaseUrl: process.env.JOINER_BASEURL
+  }
+
 async function get (id) {
 
     try {
-        const response = await axios.get("http://localhost:5008/api/Joiner?id=" + id)
+        const response = await axios.get(config.joinerBaseUrl +  "/api/Joiner?id=" + id)
         return response.data
     } catch (err) {
         console.error(err)
